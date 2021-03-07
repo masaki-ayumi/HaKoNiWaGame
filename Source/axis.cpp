7582dca1a6ba3d254820eafd8e60dfad02b577eb
DESKTOP-ROER1NG\まっちゃん <ilovegames.ilikesplatoon@gmail.com>
2021-03-08 02:55:22 +0900

    Axisクラス作成

---
+++ Source/axis.cpp
@@ -0,0 +1,15 @@
+#include "axis.h"
+
+void Axis::Draw()
+{
+	//ç¿ïWé≤ÇÃï`âÊ
+	DrawLine3D(VGet(-200, 0, 0), VGet(200, 0, 0), GetColor(255, 0, 0));
+	DrawLine3D(VGet(0, -200, 0), VGet(0, 200, 0), GetColor(0, 255, 0));
+	DrawLine3D(VGet(0, 0, -200), VGet(0, 0, 200), GetColor(0, 0, 255));
+
+
+	DrawCone3D(VGet(200, 0, 0), VGet(180, 0, 0), 5.0f, 20, GetColor(255, 0, 0), GetColor(255, 0, 0), TRUE);
+	DrawCone3D(VGet(0, 200, 0), VGet(0, 180, 0), 5.0f, 20, GetColor(0, 255, 0), GetColor(0, 255, 0), TRUE);
+	DrawCone3D(VGet(0, 0, 200), VGet(0, 0, 180), 5.0f, 20, GetColor(0, 0, 255), GetColor(0, 0, 255), TRUE);
+
+}
---
+++ Source/axis.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "../Library/gameObject.h"
+class Axis : public GameObject
+{
+public:
+	Axis(SceneBase* scene) :GameObject(scene) {};
+	~Axis() {};
+	void Draw();
+};
+
