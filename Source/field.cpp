b26b27e7243caf99310f6ffe626330a3cf9fe7ed
DESKTOP-ROER1NG\まっちゃん <ilovegames.ilikesplatoon@gmail.com>
2021-03-11 16:02:49 +0900

    空モデル描画

--- Source/field.cpp
+++ Source/field.cpp
@@ -5,10 +5,16 @@ Field::Field(SceneBase * scene)
 {
 	hModel = MV1LoadModel("data\\BoxStage.mv1");
 	assert(hModel > 0);
+	hSkyModel = MV1LoadModel("data\\Stage\\Stage00_sky.mv1");
+	assert(hSkyModel > 0);
+	//int center = MV1SearchFrame(hModel, "Cubes");
+	//MV1SetFrameUserLocalMatrix(hModel, center, MGetRotY(DX_PI_F));
 }
 
 Field::~Field()
 {
+	MV1DeleteModel(hModel);
+	MV1DeleteModel(hSkyModel);
 }
 
 void Field::Update()
@@ -18,4 +24,5 @@ void Field::Update()
 void Field::Draw()
 {
 	MV1DrawModel(hModel);
+	MV1DrawModel(hSkyModel);
 }
--- Source/field.h
+++ Source/field.h
@@ -8,4 +8,5 @@ public:
 	void Update();
 	void Draw();
 	int hModel;
+	int hSkyModel;
 };
