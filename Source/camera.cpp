ca120aa029e5e343bf6028ab49204d97edff3fd9
DESKTOP-ROER1NG\まっちゃん <ilovegames.ilikesplatoon@gmail.com>
2021-03-13 03:41:05 +0900

    縦回転追加　回転処理を関数化

--- Source/camera.cpp
+++ Source/camera.cpp
@@ -1,10 +1,12 @@
 #include "camera.h"
 #include <DxLib.h>
+#include "debugScreen.h"
 
 Camera::Camera(SceneBase * scene)
 {
 	position = VGet(100.0f, 150.f, 0);
 	target = VGet(0, 0, 0.0f);
+	rotation = VGet(1, 1, 1);
 }
 
 Camera::~Camera()
@@ -13,7 +15,7 @@ Camera::~Camera()
 
 void Camera::Update()
 {
-#if 1
+#if 0
 	if (CheckHitKey(KEY_INPUT_UP))
 	{
 		position.y += 20.0;
@@ -33,27 +35,49 @@ void Camera::Update()
 		position.z -= 20.0;
 	}
 #endif // 0
-#if 0
-
-	VECTOR add = VGet(0, 0, 0);//カメラの注視点に足すための変数
+#if 1
+	
 	//右回転
 	if (CheckHitKey(KEY_INPUT_RIGHT))
 	{
 		rotation.y += 3.0f*DX_PI_F / 180.0f;
+		Yrot();
+		//注視点を見つめる円運動をカメラ座標に代入
+		//position.x = target.x + add.x;
+		//position.y = target.y + add.y;
+		//position.z = target.z + add.z;
 	}
 	//左回転
 	if (CheckHitKey(KEY_INPUT_LEFT))
 	{
 		rotation.y -= 3.0f*DX_PI_F / 180.0f;
+		Yrot();
+		//注視点を見つめる円運動をカメラ座標に代入
+		//position.x = target.x + add.x;
+		//position.y = target.y + add.y;
+		//position.z = target.z + add.z;
+	}
+	if (CheckHitKey(KEY_INPUT_UP))
+	{
+		rotation.z += 3.0f*DX_PI_F / 180.0f;
+		Zrot();
+		//注視点を見つめる円運動をカメラ座標に代入
+		//position.x = target.x + add.x;
+		//position.y = target.y + add.y;
+		//position.z = target.z + add.z;
 	}
 
+
 	//円運動をする座標を求める
-	add.x += cosf(rotation.y)*length;
-	add.z += sinf(rotation.y)*length;
+
+	DebugSetColor(255, 255, 255);
+	DebugPrintf(0, 100, "カメラ座標X:%f,Y:%f,Z:%f",position.x,position.y,position.z);
+
 
 	//注視点を見つめる円運動をカメラ座標に代入
-	position.z = target.z + add.z;
-	position.x = target.x + add.x;
+	//position.x = target.x + add.x;
+	//position.y = target.y + add.y;
+	//position.z = target.z + add.z;
 #endif // 1
 
 }
@@ -61,4 +85,26 @@ void Camera::Update()
 void Camera::Draw()
 {
 	SetCameraPositionAndTarget_UpVecY(position, target);
+}
+
+void Camera::Yrot()
+{
+	VECTOR add = VGet(0, 0, 0);//カメラの注視点に足すための変数
+	add.x += cosf(rotation.y)*length;
+	add.z += sinf(rotation.y)*length;
+	position.x = target.x + add.x;
+	position.y = target.y + add.y;
+	position.z = target.z + add.z;
+	//return add;
+}
+
+void Camera::Zrot()
+{
+	VECTOR add = VGet(0, 0, 0);//カメラの注視点に足すための変数
+	add.x += cosf(rotation.z)*length;
+	add.y += sinf(rotation.z)*length;
+	position.x = target.x + add.x;
+	position.y = target.y + add.y;
+	position.z = target.z + add.z;
+	//return add;
 }
--- Source/camera.h
+++ Source/camera.h
@@ -8,7 +8,8 @@ public:
 	~Camera();
 	void Update();
 	void Draw();
-
+	void Yrot(); //(VECTOR add);
+	void Zrot(); //(VECTOR add);
 private:
 	VECTOR position;
 	VECTOR rotation;
