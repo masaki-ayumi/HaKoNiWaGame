1c881a762f5a0e5020b80451c9c01234ab3188a0
DESKTOP-ROER1NG\まっちゃん <ilovegames.ilikesplatoon@gmail.com>
2021-05-05 02:38:46 +0900

    地形と線分の当たり判定関数作成

--- Source/field.cpp
+++ Source/field.cpp
@@ -1,5 +1,6 @@
 #include "field.h"
 #include <assert.h>
+#include <DxLib.h>>
 
 Field::Field(SceneBase* scene)
 {
@@ -25,3 +26,13 @@ void Field::Draw()
 	MV1DrawModel(hModel);
 	MV1DrawModel(hSkyModel);
 }
+
+bool Field::CollisoinLine(VECTOR * hit, VECTOR from, VECTOR to)
+{
+	MV1_COLL_RESULT_POLY collision = MV1CollCheck_Line(hModel, -1, from, to);
+	if (collision.HitFlag == 0)
+		return false;
+	*hit = collision.HitPosition;
+	return true;
+}
+
--- Source/field.h
+++ Source/field.h
@@ -1,5 +1,8 @@
 #pragma once
 #include "../Library/gameObject.h"
+/// <summary>
+/// 地形クラス
+/// </summary>
 class Field : public  GameObject
 {
 public:
@@ -7,6 +10,15 @@ public:
 	~Field();
 	void Update();
 	void Draw();
+
+	/// <summary>
+	/// 地形と線分の当たり判定
+	/// </summary>
+	/// <param name="hit">当たった座標を返すポインタ</param>
+	/// <param name="from">線分の上限</param>
+	/// <param name="to">線分の下限</param>
+	/// <returns>当たっていたらtrueを返す</returns>
+	bool CollisoinLine(VECTOR *hit, VECTOR from, VECTOR to);
 
 	int hModel;	//地形モデル用ハンドル
 	int hSkyModel;	//空モデル用ハンドル
