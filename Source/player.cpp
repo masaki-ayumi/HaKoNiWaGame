ca87c13b819c4bb889a195fe48ea54710a44aa4a
DESKTOP-ROER1NG\まっちゃん <ilovegames.ilikesplatoon@gmail.com>
2021-05-08 18:10:23 +0900

    プレイヤーの移動処理完了　コメント記述

--- Source/player.cpp
+++ Source/player.cpp
@@ -11,7 +11,7 @@ Player::Player(SceneBase * scene):GameObject(scene)
 	int centor = MV1SearchFrame(hModel, "全ての親");
 	MV1SetFrameUserLocalMatrix(hModel, centor, MGetRotY(DX_PI_F));
 
-	position = VGet(0, 60, 0);
+	position = VGet(0, 0, 0);
 	rotation = VGet(0, 0, 0);
 }
 
@@ -22,25 +22,37 @@ Player::~Player()
 
 void Player::Update()
 {
+	//回転行列
+	MATRIX rotY = MGetRotY(rotation.y);
+
+
 	//前
 	if (CheckHitKey(KEY_INPUT_W))
 	{
-
+		//回転値に応じて0.5加速する値
+		VECTOR velocity = VTransform(VGet(0.0, 0.0, 0.5), rotY);
+		//現在の座標に加速した値を加算
+		position = VAdd(position, velocity);
 	}
 	//後ろ
 	if (CheckHitKey(KEY_INPUT_S))
 	{
-
+		//回転値に応じて-0.5加速する値
+		VECTOR velocity = VTransform(VGet(0.0, 0.0, -0.5), rotY);
+		//現在の座標に加速した値を加算
+		position = VAdd(position, velocity);
 	}
 	//左
 	if (CheckHitKey(KEY_INPUT_A))
 	{
-
+		//右旋回
+		rotation.y -= 3.0*DX_PI_F / 180.0;
 	}
 	//右
 	if (CheckHitKey(KEY_INPUT_D))
 	{
-
+		//左旋回
+		rotation.y += 3.0*DX_PI_F / 180.0;
 	}
 
 
@@ -58,6 +70,16 @@ void Player::Update()
 
 void Player::Draw()
 {
-	MV1SetPosition(hModel, position);
+	//移動行列
+	MATRIX mTranslate = MGetTranslate(position);
+	//Y軸の回転行列
+	MATRIX mRotationY = MGetRotY(rotation.y);
+	//回転してから移動
+	MATRIX matrix = MMult(mRotationY, mTranslate);
+
+
+
+	//MV1SetPosition(hModel, position);
+	MV1SetMatrix(hModel, matrix);
 	MV1DrawModel(hModel);
 }
