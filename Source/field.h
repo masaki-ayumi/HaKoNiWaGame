8c1cf8a158be925dd9ada5a8c22fccf7ab51fd3d
MatTyaN0125 <ilovegames.ilikesplatoon@gmail.com>
2021-05-11 10:02:55 +0900

    球体を用いた当たり判定のコメント記入

--- Source/field.h
+++ Source/field.h
@@ -19,6 +19,13 @@ public:
 	/// <param name="to">線分の下限</param>
 	/// <returns>当たっていたらtrueを返す</returns>
 	bool CollisoinLine(VECTOR *hit, VECTOR from, VECTOR to);
+
+	/// <summary>
+	/// 地形と球体の当たり判定
+	/// </summary>
+	/// <param name="hit">当たった時の座標返すポインタ</param>
+	/// <param name="position">プレイヤーの座標</param>
+	/// <returns>当たっていたらtrueを返す</returns>
 	bool CollisoinSphere(VECTOR *hit, VECTOR position);
 
 	int hModel;	//地形モデル用ハンドル
